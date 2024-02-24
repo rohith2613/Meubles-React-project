289c60e1e46f67781ac88caf284658c4124fdc9e
Rohith <rohithsriramula2613@gmail.com>
2024-02-24 17:33:31 +0530

    Add company name in Card

--- src/components/ProductsGrid.jsx
+++ src/components/ProductsGrid.jsx
@@ -8,7 +8,7 @@ const ProductsGrid = () => {
   return (
     <div className='pt-12 grid gap-4 md:grid-cols-2 lg:grid-cols-3 '>
       {products.map((product)=>{
-        const {title,price,image} = product.attributes;
+        const {title,price,image,company} = product.attributes;
         const rupeesAmount = formatPrice(price);
         return <Link className="card w-full shadow-xl hover:shadow-2xl transition duration-300 " key={product.id} to={`/products/${product.id}`}>
         <figure className='px-4 pt-4'>
@@ -16,6 +16,7 @@ const ProductsGrid = () => {
         </figure>
         <div className='card-body items-center text-center'>
           <h2 className='card-title capitalize tracking-wider'>{title}</h2>
+          <h4 className='capitalize text-md font-mono text-neutral text-lg'>{company}</h4>
           <span className='text-secondary '>{rupeesAmount}</span>
         </div>
         </Link>
